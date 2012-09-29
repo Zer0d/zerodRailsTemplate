89b5c266112457e753092ce51985aa39fba36485
Anna Buttfield <anna.buttfield@gmail.com>
2012-09-29 14:39:25 +1000

    Rails app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZerodRailsTemplate::Application.config.secret_token = '8eb3d62322b512b246b249a9a4c5b42fd2696dc3f00dee20ef06edfa3e367d2c064a61be50990ffffd5092b8825be902a62442de2a6037da3c8988219b4b09f2'
